0656dd1fbddf76ff3365c444cc94d0e86fc11c41
Simone Odegard <Simone.Odegard@gmail.com>
2021-03-17 16:54:16 -0700

    imported data.json to app component. created modal for image pop out. created selectedbeast.

--- src/main.js
+++ src/main.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import HornedBeast from './hornedbeasts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardDeck from 'react-bootstrap/CardDeck';
 
 class Main extends React.Component {
+  
   render() {
-    let data = require ('./assets/data.json');
 
     return (
-      <div>
-        <main>
-          {data.map ((info) => {
-            return <HornedBeast
-              title={info.title}
-              url={info.image_url}
-              description={info.description}
-              alt={info.keyword}
-              horns={info.horns}/>
-          })}
-        </main>
+      <div className="beast-container">
+        <CardDeck>
+          {this.props.cards.map ((info, index) => (
+            <div key = {index}>
+              <HornedBeast
+                index={index}
+                title={info.title}
+                url={info.image_url}
+                description={info.description}
+                alt={info.keyword}
+                horns={info.horns}
+                displayAsModal = {this.props.displayAsModal}
+                />
+            </div>
+          ))}
+        </CardDeck>
       </div>
     );
   }
