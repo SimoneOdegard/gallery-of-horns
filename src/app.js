6d34fc5f07923b2a29fc26647930e8a24f626202
Simone Odegard <Simone.Odegard@gmail.com>
2021-03-18 19:27:42 -0700

    added dropdown button that filters number of horns

--- src/app.js
+++ src/app.js
@@ -4,33 +4,47 @@ import Main from './main';
 import Footer from './footer';
 import data from './assets/data.json';
 import SelectedBeast from './selectedbeast';
+import FormInfo from './forminfo.js';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       newData: data,
+      filteredData: data,
       displayModal: false,
       favoriteBeast: {},
     }
   }
 
   displayAsModal = (index) => {
-    this.setState ({favoriteBeast: this.state.newData[index], displayModal: true});
+    this.setState ({favoriteBeast: this.state.filteredData[index], displayModal: true});
   }
 
   handleClose =() => {
     this.setState({displayModal: false});
   }
 
+  handleInput = (e) => {this.setState({ filteredData: this.state.newData.filter (value => {
+    if (e === 'all'){
+      return value;
+    } else {
+      return value.horns === Number(e);
+    }
+  })})};
+
+
   render () {
     return (
       <div className="App">
         <header>
           <Header />
+          <FormInfo 
+          handleInput={this.handleInput}
+          />
           <Main
-          cards={this.state.newData}
           displayAsModal={this.displayAsModal}
+          cards= {(this.state.filteredData) !== null ? (this.state.filteredData) : (this.state.newData)}
           />
           <SelectedBeast
           show={this.state.displayModal}
--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ class Main extends React.Component {
             </div>
           ))}
         </CardDeck>
+        
       </div>
     );
   }
